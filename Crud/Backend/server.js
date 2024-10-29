b41e1b3a700dba0a127a4fbab068b6f38eae750e
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-10-29 10:39:25 -0600

    Instalacion de Nodemon
    Instalacion de Nodemon

--- Crud/Backend/server.js
+++ Crud/Backend/server.js
@@ -1,66 +1,61 @@
-const express = require("express")
-const cors = require("cors")
-const mysql = require("mysql")
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
 
 const app = express();
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"crud"
-})
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "crud",
+});
 
-app.get("/",(req,res)=>{
-    const sql = "SELECT * FROM student"
-    db.query(sql,(err,data)=>{
-        if(err)return res.json("Error");
-        return res.json(data);
-    })
-})
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM student";
+  db.query(sql, (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
+});
 
-app.post("/create",(req,res)=>{
-    const sql ="INSERT INTO student(`name`,`email`) VALUES (?)";
-    const values =[
-        req.body.name,
-        req.body.email
-    ]
-    db.query(sql,[values],(err,data)=>{
-        if(err) return res.json("Error");
-        return res.json(data);
-    })
-})
-
+app.post("/create", (req, res) => {
+  const sql = "INSERT INTO student(`name`,`email`) VALUES (?)";
+  const values = [req.body.name, req.body.email];
+  db.query(sql, [values], (err, data) => {
+    if (err) return res.json({
+        error:true,
+        message:"Error en la insercion de los datos"
+    });
+    return res.json(data);
+  });
+});
 
 app.put("/update/:id", (req, res) => {
-    const sql = "UPDATE student SET name = ?, email = ? WHERE id =? "
+  const sql = "UPDATE student SET name = ?, email = ? WHERE id =? ";
 
-    const values = [
-        req.body.name,
-        req.body.email
-    ];
-    const id = req.params.id;
+  const values = [req.body.name, req.body.email];
+  const id = req.params.id;
 
-    db.query(sql, [...values, id], (err, data) => {
-        if (err) return res.json("Error");
-        return res.json(data);
-    });
+  db.query(sql, [...values, id], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
 });
 
 app.delete("/student/:id", (req, res) => {
-    const sql = "DELETE FROM student WHERE id = ? "
-    const id = req.params.id;
+  const sql = "DELETE FROM student WHERE id = ? ";
+  const id = req.params.id;
 
-    db.query(sql, [id], (err, data) => {
-        if (err) return res.json("Error");
-        return res.json(data);
-    });
+  db.query(sql, [id], (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
 });
 
-
-app.listen(8081, () =>{
-    console.log("Server is running on port 8081");
-})
+app.listen(8081, () => {
+  console.log("Server is running on port 8081");
+});
